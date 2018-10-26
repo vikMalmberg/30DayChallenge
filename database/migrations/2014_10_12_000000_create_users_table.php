0ce04c0a13e2f0673846a9503c06397baa89f5aa
Viktor <viktor.malmberg@officemanagement.se>
2018-10-26 20:26:34 +0200

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('points');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('challenge_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('challenge_id');
+            $table->boolean('failed');
+            $table->integer('days_completed');
+            $table->primary(['user_id', 'challenge_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('challenge_user');
+    }
+}
---
+++ database/migrations/2018_10_26_180804_create_challenges_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChallengesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenges', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('starts_at');
+            $table->date('ends_at');
+            $table->string('Name');
+            $table->integer('points');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenges');
+    }
+}
