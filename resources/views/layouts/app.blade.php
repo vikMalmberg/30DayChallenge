6f16d12c54ea49d3241daaf783c7923cf8350e54
Viktor <viktor.malmberg@officemanagement.se>
2018-11-02 23:45:47 +0100

    cleaned up navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,18 +29,15 @@
           </div>
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                placeholder
+              <a href="{{route('challenges.index')}}" class="block mt-4 lg:inline-block no-underline lg:mt-0 text-teal-lighter hover:text-white mr-4">
+                Challenges
               </a>
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                placeholder
-              </a>
-              <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
-                placeholder
+              <a href="{{route('challenges.create')}}" class="block mt-4 lg:inline-block lg:mt-0 no-underline text-teal-lighter hover:text-white mr-4">
+                Create
               </a>
             </div>
             <div>
-              <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">placeholder</a>
+              <a href="/register" class="inline-block text-sm px-4 py-2 leading-none border rounded no-underline text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Register</a>
             </div>
           </div>
         </nav>
