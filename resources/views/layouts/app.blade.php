84960eb117f2d33705abeab54cc96cb7bd0f1b19
Viktor <viktor.malmberg@officemanagement.se>
2018-11-03 23:34:44 +0100

    added login to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,10 @@
               </a>
             </div>
             <div>
-              <a href="/register" class="inline-block text-sm px-4 py-2 leading-none border rounded no-underline text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Register</a>
+              <a href="/login" class="inline-block text-sm px-4 py-2 leading-none border rounded no-underline text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4  lg:mt-0">Login</a>
+            </div>
+            <div>
+              <a href="/register" class="inline-block text-sm px-4 py-2 leading-none border rounded no-underline text-white border-white hover:border-transparent hover:text-teal hover:bg-white ml-4 mt-4 lg:mt-0">Register</a>
             </div>
           </div>
         </nav>
