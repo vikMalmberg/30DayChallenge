85867805dac41f0b0261dcaf1cfb19068556d0d3
Viktor <viktor.malmberg@officemanagement.se>
2018-11-10 16:42:14 +0100

    now being redirected to challenges after signing in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/challenges';
 
     /**
      * Create a new controller instance.
