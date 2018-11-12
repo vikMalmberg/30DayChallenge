297f8af4be4353b419bf43303935ef8023812327
Viktor <viktor.malmberg@officemanagement.se>
2018-11-12 17:52:09 +0100

    changed text above challenges to be more descriptive

--- resources/views/challenges/index.blade.php
+++ resources/views/challenges/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-<h1 class="font-hairline mb-6 text-center font-sans">Challenges</h1>
+<h1 class="font-hairline mb-6 text-center font-sans">All Challenges</h1>
 <div class="container  w-2/3 mx-auto border-teal rounded-lg shadow-lg">
     <table class="topbar w-full">
         <tr class="mb-4 bg-grey-lighter h-10 text-grey-dark font-sans">
