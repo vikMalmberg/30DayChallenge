af052d144c9b743d81d34f63ecbe2cecc4ecf5aa
Viktor <viktor.malmberg@officemanagement.se>
2018-11-26 22:26:40 +0100

    changed styling for available challenge

--- resources/views/personalChallenges/partials/available.blade.php
+++ resources/views/personalChallenges/partials/available.blade.php
@@ -1,7 +1,7 @@
 <td class="text-center pl-4 pr-4 pt-2 pb-2 border-t-2">
     <a href="{{ route('personal.challenges.update',$id)}}">
-        <button class="bg-green-lighter
-                        text-green-dark font-bold py-2
+        <button class="bg-yellow-lighter
+                        text-yellow-dark font-bold py-2
                         px-4 rounded-full
                         font-hairline">
             Check in
