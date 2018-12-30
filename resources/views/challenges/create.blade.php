aaf836398931ef6d85043147be0cbabbdb18142a
Viktor <viktor.malmberg@officemanagement.se>
2018-12-30 22:44:51 +0100

    removed old comment

--- resources/views/challenges/create.blade.php
+++ resources/views/challenges/create.blade.php
@@ -3,7 +3,6 @@
 @if ($errors->any())
   <div class="w-1/2 mx-auto h-80  justify-center items-center">
       @foreach ($errors->all() as $error)
-        {{-- <error-component emsg="123"></error-component> --}}
         <error-component :errormsg="'{{$error}}'"></error-component>
       @endforeach
   </div>
