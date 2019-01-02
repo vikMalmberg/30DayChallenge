573abb6119b1f6187d2743e4c055688179a89d13
Viktor <viktor.malmberg@officemanagement.se>
2019-01-02 05:18:22 +0100

    Made index pages more scannable

--- resources/views/personalChallenges/index.blade.php
+++ resources/views/personalChallenges/index.blade.php
@@ -14,15 +14,15 @@
 <div class="container  w-2/3 mx-auto border-teal rounded-lg shadow-lg">
     <div class="topbar w-full ">
         <div class="flex justify-around bg-grey-light rounded-t-lg">
-            <div class="text-center w-48 pl-4 pr-4 pt-2 h-10 text-grey-dark font-sans text-grey-darkest font-medium">NAME</div>
-            <div class="text-center w-24 pl-4 pr-4 pt-2 h-10 text-grey-dark font-sans text-grey-darkest font-medium">DURATION</div>
-            <div class="text-center w-24 pl-4 pr-4 pt-2 h-10 text-grey-dark font-sans text-grey-darkest font-medium">STATUS</div>
+            <div class="w-48 pt-2 mr-4 h-10 text-grey-dark font-sans text-grey-darkest font-medium">NAME</div>
+            <div class="w-24 pt-2 mr-4 h-10 text-grey-dark font-sans text-grey-darkest font-medium">DURATION</div>
+            <div class="text-center w-24 pt-2 h-10 pr-8 text-grey-dark font-sans text-grey-darkest font-medium">STATUS</div>
         </div>
 
         @foreach($challenges as $challenge)
             <div class=" hover:bg-grey pb-16 h-12 flex justify-around border-t-2">
-                <div class="text-center w-48 pt-6 pb-6 pl-4 pr-4 pt-2">{{ $challenge->name }}</div>
-                <div class="text-center w-24 pt-6 pb-6 pl-4 pr-4 pt-2 pb-2">{{ $challenge->duration() }} days</div>
+                <div class="w-48 pt-6 pb-6 pt-2">{{ $challenge->name }}</div>
+                <div class="w-24 pt-6 pb-6 pl-4 pr-4 pt-2 pb-2">{{ $challenge->duration() }} days</div>
                 @include("personalChallenges.partials.{$challenge->status}",['id' => $challenge->id])
             </div>
         @endforeach
