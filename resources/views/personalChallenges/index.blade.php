493bfc4ab477428d4117e2507f36540452585509
Viktor <viktor.malmberg@officemanagement.se>
2019-01-03 18:28:15 +0100

    indexes now show starting date for challenges too

--- resources/views/personalChallenges/index.blade.php
+++ resources/views/personalChallenges/index.blade.php
@@ -11,11 +11,12 @@
 @endif
 
 <h1 class="font-hairline mb-6 text-center font-sans">My Challenges</h1>
-<div class="container  w-2/3 mx-auto border-teal rounded-lg shadow-lg">
+<div class="container  w-4/5 mx-auto border-teal rounded-lg shadow-lg">
     <div class="topbar w-full ">
         <div class="flex justify-around bg-grey-light rounded-t-lg">
             <div class="w-48 pt-2  h-10 text-grey-dark font-sans text-grey-darkest font-medium">NAME</div>
             <div class="w-48 pt-2  h-10 text-grey-dark font-sans text-grey-darkest font-medium">DURATION</div>
+            <div class="w-48 pt-2  h-10 text-grey-dark font-sans text-grey-darkest font-medium">START</div>
             <div class="w-48 pt-2  h-10 text-grey-dark font-sans text-grey-darkest font-medium">DAYS</div>
             <div class="text-center w-32 pt-2 h-10 text-grey-dark font-sans text-grey-darkest font-medium">STATUS</div>
 
@@ -25,6 +26,7 @@
             <div class=" hover:bg-grey pb-16 h-12 flex justify-around border-t-2">
                 <div class="w-48 pt-6 pb-6 ">{{ $challenge->name }}</div>
                 <div class="w-48 pt-6 pb-6 ">{{ $challenge->duration() }} days</div>
+                <div class="w-48 pt-6 pb-6 ">{{ $challenge->starts_at }}</div>
                 <div class="w-48 pt-6 pb-6">
                     @foreach($challenge->WeekdaysToCheckIn() as $weekday)
                         <span>{{ $weekday}}</span>
